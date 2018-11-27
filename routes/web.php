d96d9c9117de8b4077da0af730838185d05e039d
Tibira <tibirasantos3b@gmail.com>
2018-11-27 14:16:27 -0200

    atualização

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,4 @@
 <?php
-<<<<<<< HEAD
-    Route::get('/','UserController@home');
-    Route::get('/wsMagia/{id}','MagiasController@ws');
-    Route::resource('users', 'UserController');
-    Route::resource('antecedentes', 'AntecedentesController');
-    Route::resource('armaduras', 'ArmadurasController');
-    Route::resource('armas', 'ArmasController');
-    Route::resource('atributos', 'AtributosController');
-    Route::resource('caracteristicas', 'CaracteristicasController');
-    Route::resource('classes', 'ClassesController');
-    Route::resource('definicoes', 'DefinicoesController');
-    Route::resource('ficha', 'FichaController');
-    Route::resource('itens', 'ItensController');
-    Route::resource('magias', 'MagiasController');
-    Route::resource('pericias', 'PericiasController');
-    Route::resource('personalizacao', 'PersonalizacaoController');
-    Route::resource('proficiencias', 'ProficienciasController');
-    Route::resource('racas', 'RacasController');
-    Route::resource('talentos', 'TalentosController');
-    
-    Auth::routes();
-    
-    // Social Auth
-    Route::get('auth/social', 'Auth\SocialAuthController@show')->name('social.login');
-    Route::get('oauth/{driver}', 'Auth\SocialAuthController@redirectToProvider')->name('social.oauth');
-    Route::get('oauth/{driver}/callback', 'Auth\SocialAuthController@handleProviderCallback')->name('social.callback');
-    Auth::routes();
-    
-    Route::get('/home', 'HomeController@index')->name('home');
-    
-=======
 
 Route::get('/', 'UserController@home');
 Route::get('/pergunta', 'FichaController@pergunta')->name('ficha.pesq');
@@ -79,4 +48,3 @@ Route::post('sair', function () {
     Auth::logout();
     return redirect('/');
 })->name('deslogar');
->>>>>>> local
